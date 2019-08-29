4bd5578e2f83d82e4a017563c0ba7805d957bf78
ggc <15555124010@163.com>
2019-08-29 18:15:00 +0800

    0

--- src/pages/demo/one/index.tsx
+++ src/pages/demo/one/index.tsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'dva';
+import { Button, notification, Card } from 'antd';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 interface Props {
   submitting: boolean;
   dispatch: Dispatch<any>;
   demoOne: any;
+}
+
+interface State {
+  value: string;
 }
 
 @connect(
@@ -22,7 +29,7 @@ interface Props {
     submitting: loading.effects['demoOne/submitRegularForm'],
   }),
 )
-class One extends Component<Props> {
+class One extends Component<Props, State> {
   hello = (e: React.FormEvent) => {
     e.preventDefault();
     console.log(123);
@@ -34,16 +41,44 @@ class One extends Component<Props> {
     });
   };
 
+  state: State = {
+    value: 'test',
+  };
+
+  handleChange = (value: any): void => {
+    this.setState({
+      value,
+    });
+  };
+
+  prompt = () => {
+    notification.open({
+      message: 'We got value:',
+      description: <span dangerouslySetInnerHTML={{ __html: this.state.value }} />,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <button onClick={this.hello}>
-          测试{this.props.submitting ? 1 : 0}
-          {this.props.demoOne.hello}
-        </button>
-      </div>
+      <Card title="富文本编辑器">
+        <ReactQuill value={this.state.value} onChange={this.handleChange} />
+        <Button style={{ marginTop: 16 }} onClick={this.prompt}>
+          Prompt
+        </Button>
+      </Card>
     );
   }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <button onClick={this.hello}>
+  //         测试{this.props.submitting ? 1 : 0}
+  //         {this.props.demoOne.hello}
+  //       </button>
+  //     </div>
+  //   );
+  // }
 }
 
 export default One;
